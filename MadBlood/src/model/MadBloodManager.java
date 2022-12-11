b6d54b8cfd724c5211bba673a219573036725a48
noslen-dev <79165069+noslen-dev@users.noreply.github.com>
2022-12-11 21:45:04 +0000

    Delete MadBlood directory

--- MadBlood/src/model/MadBloodManager.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-/**
- * This class is going to act like a proxy between the model and UI, allowing the UI
- * accessing the methods we want to allow from the model.
- */
-public class MadBloodManager {
-
-}
